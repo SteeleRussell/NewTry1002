1ee21b6b73d631e8d62d717fb6c42530349a7678
SteeleRussell <51966589+SteeleRussell@users.noreply.github.com>
2023-04-10 21:42:15 -0500

    Update movies.js

--- movies.js
+++ movies.js
@@ -109,15 +109,3 @@ var movies = [
    }
 });
 
-router.delete('/:id', function(req, res){
-   var removeIndex = movies.map(function(movie){
-      return movie.id;
-   }).indexOf(req.params.id); //Gets us the index of movie with given id.
-   
-   if(removeIndex === -1){
-      res.json({message: "Not found"});
-   } else {
-      movies.splice(removeIndex, 1);
-      res.send({message: "Movie id " + req.params.id + " removed."});
-   }
-});
